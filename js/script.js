2a1e065d63e00e532be08718d24b7396a30e3c73
hariz <hariz7801yob@gmail.com>
2023-07-07 22:15:19 +0800

    Baru

--- js/script.js
+++ js/script.js
@@ -11,10 +11,15 @@ function calculateSavings(event) {
   var totalYears = parseFloat(document.getElementById('totalYears').value);
   var yearStartSaving = parseFloat(document.getElementById('yearStartSaving').value);
   var yearEndSaving = yearStartSaving + totalYears;
-  var dividendRate = parseFloat(document.getElementById('dividendRate').value);
+  var dividendRate = parseFloat(document.querySelector('input[name="dividendRate"]:checked').value);
   var totalSavings = calculateTotalSavings(monthlySaving, dividendRate, totalYears);
+  var dividendTotal = calculateDividendTotal(monthlySaving, dividendRate, totalYears);
   var targetTotalSaving = parseFloat(document.getElementById('targetTotalSaving').value);
   var remainingTotalSaving = targetTotalSaving - totalSavings;
+  var achievedOrNot =
+    remainingTotalSaving > 0
+      ? "You need RM" + remainingTotalSaving.toFixed(2) + " more to achieve your target"
+      : "You have already achieved your target total saving";
 
   var outputList = [
     { label: "Name", value: fullName },
@@ -26,17 +31,17 @@ function calculateSavings(event) {
     { label: "Year Start Saving", value: yearStartSaving },
     { label: "Year End Saving", value: yearEndSaving },
     { label: "Dividend Rate", value: dividendRate.toFixed(2) + "%" },
+    { label: "Dividend Total", value: "RM" + dividendTotal.toFixed(2) },
     { label: "Total Savings", value: "RM" + totalSavings.toFixed(2) },
     { label: "Target Total Saving", value: "RM" + targetTotalSaving.toFixed(2) },
-    { label: "Remaining Target Total Saving", value: "RM" + remainingTotalSaving.toFixed(2) }
+    { label: "Remaining Target Total Saving", value: "RM" + remainingTotalSaving.toFixed(2) },
+    { label: "Achievement Status", value: achievedOrNot }
   ];
-
-  
 
   var resultHTML = "<h3>Calculator Result</h3>";
   resultHTML += "<ul>";
   for (var i = 0; i < outputList.length; i++) {
-    resultHTML += "<li><strong>" + outputList[i].label + ":</strong> " + outputList[i].value + "</li>";
+    resultHTML += "<strong>" + outputList[i].label + ":</strong> " + outputList[i].value + "<br>";
   }
   resultHTML += "</ul>";
 
@@ -49,10 +54,14 @@ function calculateTotalSavings(monthlySaving, dividendRate, totalYears) {
   var totalAmount = 0;
 
   for (var i = 0; i < totalYears; i++) {
-    var dividend = (totalAmount + monthlySaving) * (dividendRate / 100);
-    totalAmount += monthlySaving + dividend;
+    var dividend = (totalAmount + monthlySaving * 12 * totalYears) * (dividendRate / 100);
+    totalAmount += monthlySaving * 12 * totalYears + dividend;
   }
 
   return totalAmount;
 }
 
+function calculateDividendTotal(monthlySaving, dividendRate, totalYears) {
+  var dividend = (monthlySaving * 12 * totalYears * dividendRate) / 100;
+  return dividend;
+}
